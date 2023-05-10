d63abe7b0ba9c9288161a55055f486c8386516ce
Claudio B. Caporusso <cbcaporusso@gmail.com>
2023-05-10 14:41:01 +0200

    fix model

--- ising.py
+++ ising.py
@@ -6,34 +6,45 @@ from src.traj import Trajectories
 def main():
     
     # Define initial conditions
-    nruns = 50
+    nruns = 100
 
     t0 = 0
-    tf = 1000
+    tf = 100_000
     dt = 0.1
 
-    gamma = 0.2
+    gammas = np.linspace(0.8, 1.0, 3)
     r = 0.1
 
-    N = 10_000
+    N = 20
+    N1 = 1
 
-    # random inizitalize array to 0 or 1
-    #x0 = np.random.randint(2, size=N)
     x0 = np.zeros(N)
-    x0[10] = 1
+    # randomly initialize 100 spins to 1
+    x0[np.random.randint(0, N, size=N1)] = 1
 
+    for gamma in gammas:
+        run_simulation_almostallspin0(nruns, t0, tf, dt, x0, gamma, r,)
+
+
+def run_simulation_almostallspin0(nruns, t0, tf, dt, x0, gamma, r):
+    
+    print(f"gamma = {gamma}")
+    print(f"r = {r}")
+    
     trjs = Trajectories(t0, tf)
     for run in range(nruns):
-        print("Run {}".format(run))
+        print("Run {}".format(run), end="\r")
         sim = SpinFlip(t0, tf, dt, gamma, r, x0)
         t, m = sim.run()
         plt.plot(t, m)
         trjs.add(t, m)
 
-    t_avg, m_avg = trjs.average(10)
-    plt.plot(t_avg, m_avg, label="Average", color="black", linewidth=3)
+    t_avg, m_avg = trjs.average(50)
+    trjs.save(sim)
 
-    plt.show()
+    print('')
+    #plt.plot(t_avg, m_avg, label="Average", color="black", linewidth=3)
+    #plt.show()
 
 if __name__ == "__main__":
     main()
--- src/traj.py
+++ src/traj.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools as itertools
+import os, sys
 
 class Trajectories():
     """
@@ -51,6 +52,8 @@ class Trajectories():
         self.times.append(t)
         self.trjs.append(trj)
         self.counter += 1
+
+        self.avg = False
         
     def average(self, dt):
         """
@@ -71,4 +74,40 @@ class Trajectories():
                 m[i] += self.trjs[j][t_index]
                 m_count[i] += 1
         m = np.where(m_count > 0, m/m_count, 0)
+
+        self.t_avg = t
+        self.m_avg = m
+
+        self.avg = True
+
         return t, m
+    
+    def save(self, sim, path="data"):
+        """
+        Save the trajectories in a specific path
+        """
+
+        gamma = sim.gamma
+        r = sim.r
+
+        x0 = sim.spins
+
+        n_spins = len(x0)
+        m0 = np.mean(x0)
+
+        sim_path = f'trjs/n_{n_spins}_m0_{m0:.2f}_gamma_{gamma:.2f}_r_{r:.2f}/'
+
+        os.makedirs(sim_path, exist_ok=True)
+        os.makedirs('trjs/avgs', exist_ok=True)
+
+        # Save the trajectories
+        for i in range(self.counter):
+            np.savetxt(sim_path + f'traj_{i}.dat', np.column_stack((self.times[i], self.trjs[i])))
+
+        # Save the average
+        if self.avg:
+            np.savetxt(f'trjs/avgs/avg_n_{n_spins}_m0_{m0:.2f}_gamma_{gamma:.2f}_r_{r:.2f}.dat', np.column_stack((self.t_avg, self.m_avg)))
+        else:
+            print("Average not computed")
+            self.average(10)
+            np.savetxt(f'trjs/avgs/avg_n_{n_spins}_m0_{m0:.2f}_gamma_{gamma:.2f}_r_{r:.2f}.dat', np.column_stack((self.t_avg, self.m_avg)))
